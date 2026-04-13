88641f8f591374a16682e602a5f5659556fa5061
kondeti-chaithanya <kondeti.kumar@triconinfotech.com>
2026-04-13 13:18:41 +0530

    added vpc

---
+++ infra/terraform/modules/vpc/main.tf
@@ -0,0 +1,36 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
